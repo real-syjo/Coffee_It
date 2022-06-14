1dd7b42b2b46e59b18bb4bfd2a89c03c89fe57d1
Yuni Ji <jyhee1005@gmail.com>
2022-06-14 17:29:17 +0900

    도메인형 패키지 구조 생성

--- src/main/java/src/com/coffee/domain/model/CoffeeItApplication.java
+++ src/main/java/src/com/coffee/domain/model/CoffeeItApplication.java
@@ -1,4 +1,4 @@
-package src.com.coffee;
+package src.com.coffee.domain.model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
