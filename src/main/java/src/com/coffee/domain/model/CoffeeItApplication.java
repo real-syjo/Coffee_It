4611634276e7bc09dffed5f7874c110aaf141935
Yuni Ji <jyhee1005@gmail.com>
2022-06-21 14:00:33 +0900

    postgresql 연동 확인

--- src/main/java/src/com/coffee/domain/model/CoffeeItApplication.java
+++ src/main/java/src/com/coffee/domain/model/CoffeeItApplication.java
@@ -2,8 +2,10 @@ package src.com.coffee.domain.model;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("src.com.coffee.domain.*")
 public class CoffeeItApplication {
 
 	public static void main(String[] args) {
--- src/main/java/src/com/coffee/domain/user/api/UserApi.java
+++ src/main/java/src/com/coffee/domain/user/api/UserApi.java
@@ -1,5 +1,22 @@
 package src.com.coffee.domain.user.api;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import src.com.coffee.domain.user.domain.User;
+
+@RestController
 public class UserApi {
 
+		@RequestMapping(value="/test", method=RequestMethod.GET)
+		public ModelAndView getUser() {
+			User user = new User();
+			
+			ModelAndView modelAndView = new ModelAndView();
+			modelAndView.addObject("user", user);
+			
+			return modelAndView;
+		}
 }
